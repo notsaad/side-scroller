c03b981390d3c73ec9b405d6a39facccdc344fc6
notsaad <42980243+notsaad@users.noreply.github.com>
2018-12-02 11:47:09 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var person;
+
+//var obstacle;
+
+function setup() {
+  createCanvas(640, 360);
+  person = new Person();
+}
+
+function keyPressed() {
+
+  if (key == ' '){
+    var jump = createVector(0, -5);
+    person.applyForce(jump);
+  }
+
+}
+
+function draw() {
+  background(51);
+
+  var gravity = createVector(0, 0.1);
+  person.applyForce(gravity);
+
+  translate(-person.pos.x+50, 0);
+
+  person.update();
+  person.edges();
+  person.display();
+
+  fill(255, 0, 100);
+  rect(400, height-50, 50, 50);
+}
